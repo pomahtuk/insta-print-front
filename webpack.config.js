3420bcb9ba11b6a0666c06e7beb3089ad98904ec
Ilya Lovriakov <ilya.lovriakov@booking.com>
2015-07-13 15:56:00 +0200

    initial setup

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var webpack = require('webpack');
+
+module.exports = {  
+    entry: [
+      'webpack/hot/only-dev-server',
+      "./js/app.jsx"
+    ],
+    output: {
+        path: __dirname + '/build',
+        filename: "bundle.js"
+    },
+    module: {
+      loaders: [
+        { test: /\.js?$/, loaders: ['react-hot', 'babel'], exclude: /node_modules/ },
+        { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'},
+        { test: /\.css$/, loader: "style!css" },
+        // SASS
+        { test: /\.scss$/, loader: 'style!css!sass' }
+      ]
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.NoErrorsPlugin()
+    ]
+
+};
