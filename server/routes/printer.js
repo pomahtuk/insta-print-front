24a814377eaeaa5f97f9ff675ed4eecf8f788f4b
Ilya Lovriakov <ilya.lovriakov@booking.com>
2015-08-17 16:05:21 +0200

    basic pdf generation

--- server/routes/printer.js
+++ server/routes/printer.js
@@ -1,10 +1,32 @@
 const router = require('koa-router')();
+const PDFDocument = require('pdfkit');
+const request = require('koa-request').defaults({ encoding: null });
+const fs = require('fs');
+const path = require('path');
+const os = require('os');
+const mime = require('mime');
 
 // pass everything to react
 router.get('/printer', function* () {
+  var doc = new PDFDocument({
+    size: 'LEGAL',
+    layout: 'landscape'
+    // // thouse in  millimeters
+    // page_width: '1682',
+    // page_height: '2378'
+  });
+
+  var url = 'https://igcdn-photos-f-a.akamaihd.net/hphotos-ak-xaf1/t51.2885-15/11881798_104299353258093_2104323877_n.jpg';
+  var img = yield request(url);
+
+  doc.image(img.body, 320, 145, {width: 200, height: 100});
+
+  doc.end();
+
   try {
     this.status = 200;
-    this.body = 'printer';
+    this.type = 'application/pdf';
+    this.body = doc;
   } catch (err) {
     this.throw(err);
   }
