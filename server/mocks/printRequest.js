d01e1e305ebe7261eb920df92d046a58f4f34571
Ilya Lovriakov <ilya.lovriakov@booking.com>
2015-08-19 09:25:58 +0200

    pdf generation mockups

---
+++ server/mocks/printRequest.js
@@ -0,0 +1,50 @@
+const printData = [
+  {
+    'data': {
+      'type': 'image',
+      'filter': 'Walden',
+      'tags': [],
+      caption: {
+        created_time: '1439751928',
+        id: '1053037964612968823',
+        text: 'Нет покоя даже вечером воскресенья. #raspberrypi #coding #coinacceptor'
+      },
+      'link': 'http://instagr.am/p/D/',
+      'images': {
+        'standard_resolution': {
+          'url': 'http://distillery.s3.amazonaws.com/media/2010/07/16/4de37e03aa4b4372843a7eb33fa41cad_7.jpg',
+          'width': 612,
+          'height': 612
+        }
+      },
+      'id': '3',
+      'location': null
+    }
+  },
+
+  {
+    'data': {
+      'type': 'video',
+      'filter': 'Vesper',
+      'tags': [],
+      caption: {
+        created_time: '1437826527',
+        id: '1036886593178280508',
+        text: 'Ахаха Амстердам, что ты делаешь? Прекрати! #amsterdam #weather #weekend'
+      },
+      'link': 'http://instagr.am/p/D/',
+      'created_time': '1279340983',
+      'images': {
+        'standard_resolution': {
+          'url': 'https://igcdn-photos-c-a.akamaihd.net/hphotos-ak-xaf1/t51.2885-15/11374758_562160623923178_1798989365_n.jpg',
+          'width': 612,
+          'height': 612
+        }
+      },
+      'id': '4',
+      'location': null
+    }
+  }
+];
+
+module.exports = printData;
--- server/routes/printer.js
+++ server/routes/printer.js
@@ -4,25 +4,149 @@ const request = require('koa-request').defaults({ encoding: null });
 const fs = require('fs');
 const path = require('path');
 const os = require('os');
+const co = require('co');
+
+// set conversion values
+const mmToPixel = 3.779527559055;
+const mmToInch = 0.0393700787;
+// sizes setup
+const imageSize = 66.2 * mmToPixel;
+const textHeight = 20.4 * mmToPixel;
+const margin = 5 * mmToPixel;
+const pageHeight = 101.6 * mmToPixel;
+const pageWidth = 152.4 * mmToPixel;
+
+// mock data
+var printData = require('../mocks/printRequest.js');
+
+const fontRegularPath = path.normalize(path.join(__dirname, '/../../client/fonts/Roboto-Regular.ttf'));
+
+// allow iterate over object making async calls
+function *mapGen (arr, callback) {
+  for (var i = 0; i < arr.length; i++) {
+    yield callback(arr[i], i);
+  }
+}
 
 // pass everything to react
 router.get('/printer', function* () {
   var doc = new PDFDocument({
-    size: 'LEGAL',
-    layout: 'landscape'
-    // // thouse in  millimeters
-    // page_width: '1682',
-    // page_height: '2378'
+    layout: 'landscape',
+    size: [pageHeight, pageWidth]
   });
 
-  var url = 'https://igcdn-photos-f-a.akamaihd.net/hphotos-ak-xaf1/t51.2885-15/11881798_104299353258093_2104323877_n.jpg';
-  var img = yield request(url);
+  doc.registerFont('Roboto', fontRegularPath);
 
-  doc.image(img.body, 320, 145, {width: 200, height: 100});
+  var printDataSize = printData.length - 1;
 
-  doc.end();
+  function recurseExtract(text, searchArr, i, container) {
+    // no text left
+    if (!text) return;
 
+    var search = searchArr[i];
+    var splittingArr = text.split(search);
+    var index = text.indexOf(search);
+
+    if (index === 0) {
+      container.push({
+        text: search,
+        type: 'tag'
+      });
+      container.push({
+        text: splittingArr[0],
+        type: 'text'
+      });
+    }else if (index !== 0) {
+      container.push({
+        text: splittingArr[0],
+        type: 'text'
+      });
+      container.push({
+        text: search,
+        type: 'tag'
+      });
+    }
+
+    recurseExtract(splittingArr[1], searchArr, i += 1, container);
+
+  }
+
+  function extractHashTags(text) {
+    // here we have a hashtags
+    var hashTagsArray = text.match(/#\w+/g);
+    var resultArray = [];
+
+    recurseExtract(text, hashTagsArray, 0, resultArray);
+
+    return resultArray;
+  }
+
+  function* drawImage (image, index) {
+    image = image.data;
+
+    var divisionLeftover = index % 2;
+    var shouldCreateNewPage = printDataSize - index > 0 && divisionLeftover === 1;
+    var leftOffset = margin + (2 * margin + imageSize) * divisionLeftover;
+
+    var url = image.images.standard_resolution.url;
+    var img = yield request(url);
+
+    // may fall with image body - make a fallback
+    // use some empty image?
+    doc.image(img.body, leftOffset, margin, {width: imageSize});
+
+    // get text and extract hastags
+    var origText = image.caption ? image.caption.text : '';
+    var textsArray = extractHashTags(origText);
+
+    doc.font('Roboto');
+    doc.fontSize(11);
+    doc.moveTo(0, 0);
+
+    var currentText = doc.text('', leftOffset, imageSize + (2 * margin), {
+      width: imageSize,
+      height: textHeight,
+      continued: true
+    });
+
+    textsArray.forEach(function(textItem) {
+      if (textItem.type === 'text') {
+        currentText = currentText.fillColor('black');
+      } else if (textItem.type === 'tag') {
+        currentText = currentText.fillColor('blue');
+      }
+
+      currentText = currentText.text(textItem.text, {
+        continued: true
+      });
+    });
+
+    // reset text, nothing will be added
+    currentText = currentText.text('', {
+      continued: false
+    });
+
+    // here comes the text
+
+    if (shouldCreateNewPage) {
+      doc.addPage();
+    }
+  }
   try {
+
+    yield* mapGen(printData, drawImage);
+
+    // set cutting line for now
+    doc
+      .lineCap('butt')
+      .moveTo(2 * margin + imageSize, 5)
+      .lineTo(2 * margin + imageSize, pageHeight)
+      .dash(5, {space: 5})
+      .strokeColor('#eee')
+      .stroke();
+
+    doc.end();
+
     this.status = 200;
     this.type = 'application/pdf';
     this.body = doc;
