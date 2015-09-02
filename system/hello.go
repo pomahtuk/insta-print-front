a69b9e0329ffb2e2a39b50c4c9958e286a3fef22
Ilya Lovriakov <ilya.lovriakov@booking.com>
2015-09-02 18:54:43 +0200

    wip

--- system/hello.go
+++ system/hello.go
@@ -1,41 +1,43 @@
 package main
 
-import "fmt"
-import "database/sql"
-import _ "github.com/go-sql-driver/mysql"
-import "time"
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"time"
+)
 
 func main() {
-  db, err := sql.Open("mysql", "root:@/printbox_dev?charset=utf8mb4,utf8&parseTime=true")
-  checkErr(err)
+	db, err := sql.Open("mysql", "root:@/printbox_dev?charset=utf8mb4,utf8&parseTime=true")
+	checkErr(err)
 
-  defer db.Close()
+	defer db.Close()
 
-  // Open doesn't open a connection. Validate DSN data:
-  err = db.Ping()
-  checkErr(err)
+	// Open doesn't open a connection. Validate DSN data:
+	err = db.Ping()
+	checkErr(err)
 
-  // query
-  rows, err := db.Query("SELECT * FROM Events")
-  checkErr(err)
+	// query
+	rows, err := db.Query("SELECT * FROM Events")
+	checkErr(err)
 
-  for rows.Next() {
-    var id int
-    var eventType string
-    var timeStamp time.Time
-    var data string
-    var createdAt time.Time
-    var updatedAt time.Time
+	for rows.Next() {
+		var id int
+		var eventType string
+		var timeStamp time.Time
+		var data string
+		var createdAt time.Time
+		var updatedAt time.Time
 
-    err = rows.Scan(&id, &eventType, &timeStamp, &data, &createdAt, &updatedAt)
-    checkErr(err)
+		err = rows.Scan(&id, &eventType, &timeStamp, &data, &createdAt, &updatedAt)
+		checkErr(err)
 
-    fmt.Println(id)
-  }
+		fmt.Println(id)
+	}
 }
 
 func checkErr(err error) {
-  if err != nil {
-    panic(err)
-  }
+	if err != nil {
+		panic(err)
+	}
 }
